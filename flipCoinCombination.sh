b2c7e27c34088f4a394621e25cf486bee500a16a
yamini <vundelayamini@gmail.com>
2021-02-14 18:31:59 +0530

    Added : to print head or tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,12 @@
-#welcome message to winning the percentage of head or tail combination in singlet,doublet,triplet to problem solution
+#!/bin/bash -x
+
+flipCoin=$((RANDOM%2))
+isHeads=1
+
+if [ $flipCoin -eq $isHeads ]
+then
+      echo HEADS
+else
+      echo TAILS
+fi
+

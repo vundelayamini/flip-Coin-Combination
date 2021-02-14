1b8d88a4c633c506ca97699c17a7ba591ce5c400
yamini <vundelayamini@gmail.com>
2021-02-14 19:26:34 +0530

    Added :  result of singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,12 +1,29 @@
 #!/bin/bash -x
+Head=0
+Tail=0
+declare -A singlet
 
-flipCoin=$((RANDOM%2))
-isHeads=1
+function percentagesinglet() {
+              percentageHead=$(( ${singlet[H]}/10*100 ))
+               echo $percentageHead
+                 percentageTail=$(( ${singlet[H]}/10*100 ))
+                     echo $percentageTail
+}
+for ((i=1; i<=10; i++))
+do
+   flipCoin=$((RANDOM%2))
 
-if [ $flipCoin -eq $isHeads ]
-then
-      echo HEADS
-else
-      echo TAILS
-fi
-
+    if [ $flipCoin==1 ]
+      then
+         echo "Head"
+           ((countHead++))
+          singlet[H]=$Head
+      else
+         echo "Tail"
+         ((countTail++))
+         singlet[T]=$Tail
+    fi
+done
+echo $Head
+echo $Tail
+echo "[ ${singlet[@]} ]"
